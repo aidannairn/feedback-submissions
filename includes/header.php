e6d48c904d91d844cf24f4097a2931337a28090b
aidannairn <aidan.nairn95@gmail.com>
2023-01-26 09:28:25 +1300

    feat: Convert HTML files to PHP. Use templating to include headers and footers.

--- includes/header.php
+++ includes/header.php
@@ -17,13 +17,13 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
          <li class="nav-item">
-           <a class="nav-link" href="index.html">Home</a>
+           <a class="nav-link" href="index.php">Home</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link" href="feedback.html">Feedback</a>
+           <a class="nav-link" href="feedback.php">Feedback</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link" href="about.html">About</a>
+           <a class="nav-link" href="about.php">About</a>
          </li>
         </ul>
       </div>
@@ -32,17 +32,3 @@
 
 <main>
   <div class="container d-flex flex-column align-items-center">
-    <h2>About</h2>
-
-    <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?</p>
-    </div>
-</main>
-
-<footer class="text-center mt-5">
-  Copyright &copy; 2023
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
